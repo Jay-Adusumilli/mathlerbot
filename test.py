434761bd052c7229728b8e18b737eb7ef40e7e42
maheedhar1998 <maheedhar1998@hotmail.com>
2022-02-13 03:59:22 -0500

    Regex Tester

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-chars = [111111,222222,333333,444444]
+import re
+chars = ["111111","222222","333333","444444","5555555", "6666666", "77777777"]
+no_chars = ['1','2','6']
+yes_chars = ['3', '4']
 
+x = re.compile('^['+"".join(no_chars)+']+$')
+y = re.compile('^['+"".join(yes_chars)+']+$')
+for i in chars:
+	if re.search(x,i) or not re.search(y,i):
+		print("Valid: ", i)
