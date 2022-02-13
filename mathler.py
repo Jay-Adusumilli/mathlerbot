97123540fced18c350ba7364d42e51ae01c41bf2
Jay-Adusumilli <jyotiswaroop.adusumilli@gmail.com>
2022-02-13 03:20:43 -0500

    Add current files

---
+++ mathler.py
@@ -0,0 +1,72 @@
+import sys
+from time import sleep
+import os
+from sympy import sympify
+import re
+
+if len(sys.argv) != 2:
+    print("Useage: python mathler.py input.txt")
+    quit()
+name = sys.argv[1]
+with open(name, "r") as fp:
+    answer_list = fp.read().splitlines()
+
+chars = []
+notchars = []
+spaces = len(answer_list[0])
+final = [None] * spaces
+
+def printanswers():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("\nNum guesses: " + str(len(answer_list)) + "\n")
+    if len(answer_list) > 60:
+        trunc = answer_list[:60] + ["...","...","...","...","...","..."]
+        x = 1
+    else:
+        trun = answer_list
+    for a,b,c,d,e,f in zip(trunc[::6],trunc[1::6],trunc[2::6],trunc[3::6],trunc[4::6],trunc[5::6]):
+        print ('{:<10}{:<10}{:<10}{:<10}{:<10}{:<}'.format(a,b,c,d,e,f))
+        print("")
+        sleep(0.05)
+    print("")
+    
+
+def removeanswers():
+    temp_list = []
+    new = ""
+    [new.join(i) for i in chars]
+    x = re.compile('^['+new+']+$')
+    for i,index in answer_list:
+         if x.match(i) == None:
+             answer_list.remove(index)
+             index -= 1
+
+
+while (len(answer_list) > 1):
+    try:
+        printanswers()
+        first = input("Input: ")
+        firstc = input("Colors [g/y/r]: ")
+
+        for i in range(len(firstc)):
+            if firstc[i] == 'g':
+                final[i] = first[i]
+                chars.append(first[i])
+            if firstc[i] == 'r':
+                notchars.append(first[i])
+            if firstc[i] == 'y':
+                chars.append(first[i])
+        print(chars)
+        print(notchars)
+        print(final)
+        print(len(answer_list))
+
+        removeanswers()
+        print("here")
+        print(len(answer_list))
+
+    except KeyboardInterrupt:
+        print("\nQuiting...")
+        quit()
+
+
---
+++ validlist.py
@@ -0,0 +1,63 @@
+from itertools import product
+from sympy import sympify
+from tqdm import tqdm
+import multiprocessing as mp
+import os
+
+chars = "0123456789+-*/"
+spaces = 6
+answer = 15
+oper = ["+", "-", "*", "/"]
+non_zeros = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+answer_list = []
+
+
+def isValid(exp):
+    has_ops = 0
+    if exp[0] == "0":
+        return None
+    if exp[0] in oper or exp[spaces - 1] in oper:
+        return None
+    for i in range(len(exp) - 1):
+        if exp[i] in oper:
+            has_ops = 1
+        if exp[i] in oper and exp[i + 1] in oper:
+            return None
+        if exp[i] in oper and exp[i + 1] == "0":
+            return None
+    if has_ops == 0:
+        return None
+    return exp
+    
+
+def equalsAnswer(temp):
+    if sympify(temp) == answer:
+        return temp
+
+
+if __name__ == "__main__":
+    all_valid = []
+    total = (len(chars) ** spaces)
+    cores = mp.cpu_count()
+    print("\nFiltering all valid strings:")
+    with mp.Pool(processes=cores) as pool:
+        for p in tqdm(pool.imap_unordered(func = isValid,iterable = product(chars, repeat = spaces), chunksize = 1000), total = total):
+            all_valid.append(p)
+
+    all_valid[:] = ["".join(x) for x in all_valid if x != None]
+
+    print("\nGetting all answers to " + str(answer) + ":")
+    with mp.Pool(processes=cores) as pool:
+        for a in tqdm(pool.imap_unordered(func = equalsAnswer,iterable = all_valid, chunksize = 1000), total = len(all_valid)):
+            answer_list.append(a)
+
+    answer_list[:] = [x for x in answer_list if x != None]
+    name = str(answer) + "_" + str(spaces) + "_valid_exp.txt"
+    fp = open(name, "w")
+    print("\nOutputting file: ")
+    for i in tqdm(answer_list, total = len(answer_list)):
+        temp = i + "\n"
+        fp.write(temp)
+
+    fp.close()
